33920d8c35f374b1a5d69db966a9abb473b07d75
Josua Simatupang <94213206+boogerjosh@users.noreply.github.com>
2023-10-17 15:02:23 +0700

    Update format error text

--- src/modules/form/views/index.tsx
+++ src/modules/form/views/index.tsx
@@ -414,41 +414,43 @@ const FormView = () => {
                         css={containerStyle.labelText}
                       >
                         Number {index + 1}
-                        <div style={{ display: "flex", alignItems: "center", width: '100%', marginBottom: '0.75rem' }}>
-                          <input
-                            css={containerStyle.inputField}
-                            type="text"
-                            id={`number${index}`}
-                            name={`number${index}`}
-                            tabIndex={2 + index}
-                            placeholder="Number"
-                            value={phoneNumber}
-                            onChange={handlePhoneNumberChange(index)}
-                          />
-                          {index !== 0 && (
-                            <button
-                              type="button"
-                              css={containerStyle.deleteButton}
-                              onClick={() => handleDeleteInputNumber(index)}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                x="0px"
-                                y="0px"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 30 30"
+                        <div>
+                            <div style={{ display: "flex", alignItems: "center", width: '100%', marginBottom: '0.75rem' }}>
+                            <input
+                              css={containerStyle.inputField}
+                              type="text"
+                              id={`number${index}`}
+                              name={`number${index}`}
+                              tabIndex={2 + index}
+                              placeholder="Number"
+                              value={phoneNumber}
+                              onChange={handlePhoneNumberChange(index)}
+                            />
+                            {index !== 0 && (
+                              <button
+                                type="button"
+                                css={containerStyle.deleteButton}
+                                onClick={() => handleDeleteInputNumber(index)}
                               >
-                                <path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"></path>
-                              </svg>
-                            </button>
-                          )}
-                        </div>
-                        {errorsMessage[`phoneNumber${index}`] && (
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  x="0px"
+                                  y="0px"
+                                  width="20"
+                                  height="20"
+                                  viewBox="0 0 30 30"
+                                >
+                                  <path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"></path>
+                                </svg>
+                              </button>
+                            )}
+                          </div>
+                          {errorsMessage[`phoneNumber${index}`] && (
                           <div css={containerStyle.errorMessage}>
                             {errorsMessage[`phoneNumber${index}`]}
                           </div>
                         )}
+                        </div>
                       </label>
                     </div>
                   ))}
